75ef9a2a80234113a9ff5e3c325f715727054d7e
Trek25 <florian.ricour@student.ulg.ac.be>
2018-02-06 15:13:58 +0100

    quick implementation

--- ArgoPrepareforDiva.R
+++ ArgoPrepareforDiva.R
@@ -1,4 +1,18 @@
-ArgoPrepareforDiva<- function(){
-  
-  
+# IN 
+# pars: a vector of parameters chosen by the user for DIVAND
+# initdf : initial data frame to process
+#
+# OUT
+# a text file named ArgoforDiva.txt
+
+ArgoPrepareforDiva<- function(pars, initdf){
+
+df <- subset(initdf, select=pars)  
+
+#remove rows containing NA's
+df <- df[complete.cases(df),]
+
+write.table(df, file="ArgoforDiva.txt", sep=" ", na = "NA", dec = ".", eol = "\r\n",
+                            row.names = FALSE, col.names = FALSE)    
+
 }
